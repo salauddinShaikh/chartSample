903f6eb41e741a985ed1703f092f558231ab1ce9
SagarShelar <sagar.shelar@eternussolutions.com>
2016-08-24 19:51:11 +0530

    Added heat and tree chart

--- components/advanceChart.js
+++ components/advanceChart.js
@@ -5,6 +5,8 @@ import * as _ from 'lodash';
 import Funnel from 'highcharts/modules/funnel.src';
 import Exporting from 'highcharts/modules/exporting';
 import Highcharts from 'highcharts';
+import Heatmap from 'highcharts/modules/heatmap'; 
+import Treemap from 'highcharts/modules/treemap'; 
 
 
 class AdvanceChart extends Component {
@@ -229,6 +231,108 @@ class AdvanceChart extends Component {
                         [180.3, 83.2], [180.3, 83.2]]
                 }]
     
+            },
+             optionsHeat : {
+                 chart: {
+                    renderTo: 'container',
+                    type: 'heatmap',
+                    marginTop: 40,
+                    marginBottom: 80
+                },
+        
+        
+                title: {
+                    text: 'Sales per employee per weekday'
+                },
+        
+                xAxis: {
+                    categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
+                },
+        
+                yAxis: {
+                    categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                    title: null
+                },
+        
+                colorAxis: {
+                    min: 0,
+                    minColor: '#FFFFFF',
+                    maxColor: Highcharts.getOptions().colors[0]
+                },
+        
+                legend: {
+                    align: 'right',
+                    layout: 'vertical',
+                    margin: 0,
+                    verticalAlign: 'top',
+                    y: 25,
+                    symbolHeight: 280
+                },
+        
+                // optional make  better tooltip
+        
+                tooltip: {
+                    formatter: function () {
+                        return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
+                            this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
+                    }
+                },
+        
+                series: [{
+                    name: 'Sales per employee',
+                    borderWidth: 1,
+                    data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000'
+                    }
+                }]
+            },
+            optionsTree : {
+                 
+                 chart: {
+                     renderTo: 'container'
+                 },
+                 colorAxis: {
+                    minColor: '#FFFFFF',
+                    maxColor: Highcharts.getOptions().colors[0]
+                },
+                series: [{
+                    type: "treemap",
+                    //layoutAlgorithm: 'strip',
+                    data: [{
+                        name: 'Windows 7',
+                        value: 6,
+                        colorValue: 1
+                    }, {
+                        name: 'OSX',
+                        value: 6,
+                        colorValue: 2
+                    }, {
+                        name: 'Chromebook',
+                        value: 4,
+                        colorValue: 3
+                    }, {
+                        name: 'Firefox OS',
+                        value: 3,
+                        colorValue: 4
+                    }, {
+                        name: 'Android',
+                        value: 2,
+                        colorValue: 5
+                    }, {
+                        name: 'Safari',
+                        value: 2,
+                        colorValue: 6
+                    }, {
+                        name: 'Ubuntu',
+                        value: 1,
+                        colorValue: 7
+                    }]
+                }],
+                title: {
+                    text: 'Visitor OS Breakdown'
+                }
             }
         });
     }
@@ -243,6 +347,14 @@ class AdvanceChart extends Component {
                         </div>
                          <div className="col-md-6">
                             <Chart container="optionsScatter" options={this.state.optionsScatter} modules={[Exporting]}/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Chart container="optionsHeat" options={this.state.optionsHeat} modules={[Heatmap]}/>
+                        </div>
+                         <div className="col-md-6">
+                            <Chart container="optionsTree" options={this.state.optionsTree} modules={[Treemap]}/>
                         </div>
                     </div>
                 </div>
